ad752c41faf0076520fadc036e35f7ac6e4727a2
take8 <37664118+take8@users.noreply.github.com>
2019-01-07 02:08:01 +0900

    baseUrlの設定を修正

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 // https://cli.vuejs.org/config/
 module.exports = {
-  // baseUrl: "/static/",
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/vuejs-todo-app/'
+    : '/',
   outputDir: 'docs'
   // indexPath: "../src/client_side/index.tt"
 }
